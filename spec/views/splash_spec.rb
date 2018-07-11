1b292a72eabfce6a734875f5549902d5b9384ea2
Esha Datta <edatta@crossref.org>
2018-07-10 20:28:35 -0400

    added more tests, changed verbiage of description

--- spec/views/splash_spec.rb
+++ spec/views/splash_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
-describe "actions with the splash page", vcr: true do
-  #context "renders the splash page with a search bar and the number of indexed works" do
+describe "actions on the splash page", vcr: true do
+    search_bar = "#search-input"
     before(:each) do
       visit '/'
     end
-    it 'should render' do
+    it 'renders' do
       expect(page.status_code).to be 200
     end
-    it 'should have a search bar' do
+    it 'has a search bar' do
       expect(page).to have_selector("#search-input")
     end
-    it 'should have a number of indexed works in the text' do
+    it 'has a number of indexed works in the text' do
       number = page.find("//span[@class = 'number']").text.to_i
       expect(number).to be > 0
     end
-  #end
+
 end
